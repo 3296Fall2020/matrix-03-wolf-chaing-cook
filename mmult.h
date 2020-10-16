2b9af379a24c5ea000ea12b19d91a6e1e8e53b41
Aley L Chaing <tuh15661@chinook.cis.temple.edu>
2020-10-16 01:27:01 -0400

    for task 3

---
+++ mmult.h
@@ -0,0 +1,6 @@
+/*This is a header file for mmult*/
+
+
+int mmult(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols);
+int mmult_vector(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols);
+
